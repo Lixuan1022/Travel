91f6f608bedaa3c8816b495860d8f2f9b635defb
lixuan <810371005@qq.com>
2019-10-15 16:47:00 +0800

    optimize

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,9 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list :cities="cities" :hot="hotCities"></city-list>
-        <city-alphabet :cities="cities"></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+        <city-alphabet @change="handleLetterChange"
+        :cities="cities"></city-alphabet>
     </div>
         
     
@@ -28,7 +29,8 @@ export default {
     data (){
         return{
             cities:{},
-            hotCities:[]
+            hotCities:[],
+            letter:''
         }
     },
     methods:{
@@ -43,6 +45,9 @@ export default {
                 this.cities = data.cities
                 this.hotCities = data.hotCities
             }
+        },
+        handleLetterChange (letter) {
+            this.letter= letter
         }
     },
     mounted () {
